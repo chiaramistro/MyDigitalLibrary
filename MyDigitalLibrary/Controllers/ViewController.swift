0ccb3ae195926d446b967ec8accf5e11cc8808bc
Chiara Mistrorigo <c.mistrorigo@reply.it>
2024-04-02 09:36:36 +0200

    Init project

---
+++ MyDigitalLibrary/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDigitalLibrary
+//
+//  Created by Chiara Mistrorigo on 02/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
