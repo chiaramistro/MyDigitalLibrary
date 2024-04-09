3e280b35366a0c65398a338098617fbf618aaeba
Chiara Mistrorigo <c.mistrorigo@reply.it>
2024-04-09 14:50:42 +0200

    Avoid adding authors and books twice

--- MyDigitalLibrary/Controllers/Search/SearchViewController+TableViewController.swift
+++ MyDigitalLibrary/Controllers/Search/SearchViewController+TableViewController.swift
@@ -28,7 +28,7 @@ extension SearchViewController {
     
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
         let favorite = UITableViewRowAction(style: .normal, title: "Favorite") { action, index in
-            switch (self.type as! SearchEnum) {
+            switch (self.type!) {
             case SearchEnum.book:
                 print("Add book to favourites")
                 self.alertAuthor { includeAuthor in
@@ -37,15 +37,19 @@ extension SearchViewController {
             case SearchEnum.author:
                 print("Add author to favourites")
                 let selectedResult = self.searchResults[(indexPath as NSIndexPath).row]
-                let author = Author(context: self.dataController.viewContext)
-                author.key = selectedResult.descriptionKey
-                author.photoKey = selectedResult.imageKey
-                author.name = selectedResult.title
+                let authorFound = self.checkExistingAuthor(authorKey: selectedResult.descriptionKey )
+                if let authorFound = authorFound {
+                    debugPrint("Author already present in favourite list")
+                    self.showToast(message: "Author already present in favourite list")
+                } else {
+                    let author = Author(context: self.dataController.viewContext)
+                    author.key = selectedResult.descriptionKey
+                    author.photoKey = selectedResult.imageKey
+                    author.name = selectedResult.title
+                }
                 try? self.dataController.viewContext.save()
                 self.showToast(message: "New author saved successfully")
                 debugPrint("New author saved successfully")
-            default:
-                debugPrint("Unknown type")
             }
         }
         
@@ -71,29 +75,35 @@ extension SearchViewController {
     
     func addBook(includeAuthor: Bool, indexPath: IndexPath) {
         let selectedResult = self.searchResults[(indexPath as NSIndexPath).row]
-        let book = Book(context: self.dataController.viewContext)
-        book.key = selectedResult.descriptionKey
-        book.coverKey = selectedResult.imageKey
-        book.title = selectedResult.title
-        book.author = selectedResult.bookAuthorName
-        if (includeAuthor) {
-            let authorFound = self.checkForAuthor(authorKey: selectedResult.bookAuthorKey ?? "")
-            if let authorFound = authorFound {
-                book.authorData = authorFound
-            } else {
-                let author = Author(context: self.dataController.viewContext)
-                author.key = selectedResult.bookAuthorKey
-                author.photoKey = selectedResult.bookAuthorKey
-                author.name = selectedResult.bookAuthorName
-                book.authorData = author
+        let bookFound = self.checkExistingBook(bookKey: selectedResult.descriptionKey)
+        if let bookFound = bookFound {
+            debugPrint("Book already present in favourite list")
+            self.showToast(message: "Book already present in favourite list")
+        } else {
+            let book = Book(context: self.dataController.viewContext)
+            book.key = selectedResult.descriptionKey
+            book.coverKey = selectedResult.imageKey
+            book.title = selectedResult.title
+            book.author = selectedResult.bookAuthorName
+            if (includeAuthor) {
+                let authorFound = self.checkExistingAuthor(authorKey: selectedResult.bookAuthorKey ?? "")
+                if let authorFound = authorFound {
+                    book.authorData = authorFound
+                } else {
+                    let author = Author(context: self.dataController.viewContext)
+                    author.key = selectedResult.bookAuthorKey
+                    author.photoKey = selectedResult.bookAuthorKey
+                    author.name = selectedResult.bookAuthorName
+                    book.authorData = author
+                }
             }
+            try? self.dataController.viewContext.save()
+            self.showToast(message: "New book saved successfully")
+            debugPrint("New book saved successfully")
         }
-        try? self.dataController.viewContext.save()
-        self.showToast(message: "New book saved successfully")
-        debugPrint("New book saved successfully")
     }
     
-    func checkForAuthor(authorKey: String) -> Author? {
+    func checkExistingAuthor(authorKey: String) -> Author? {
         let fetchRequest: NSFetchRequest<Author> = Author.fetchRequest()
         let predicate = NSPredicate(format: "key == %@", authorKey)
         fetchRequest.predicate = predicate
@@ -112,6 +122,30 @@ extension SearchViewController {
             }
         } catch let error as NSError {
             debugPrint("Could not fetch favourite author \(error.localizedDescription)")
+            return nil
+        }
+        
+    }
+    
+    func checkExistingBook(bookKey: String) -> Book? {
+        let fetchRequest: NSFetchRequest<Book> = Book.fetchRequest()
+        let predicate = NSPredicate(format: "key == %@", bookKey)
+        fetchRequest.predicate = predicate
+        let sortDescriptor = NSSortDescriptor(key: "key", ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        fetchRequest.fetchLimit = 1
+        
+        do {
+            let result: [Book] = try self.dataController.viewContext.fetch(fetchRequest)
+            if (result.isEmpty) { // no matching object
+                print("Book is NOT already present")
+                return nil
+            } else { // at least one matching object exists
+                print("Book is already present")
+                return result.first
+            }
+        } catch let error as NSError {
+            debugPrint("Could not fetch favourite book \(error.localizedDescription)")
             return nil
         }
         
